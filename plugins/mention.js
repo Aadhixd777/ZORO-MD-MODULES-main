f32cbc9d7dd2927efa9771adb2be6f259152ec5b
Aadhi Xd ofc <aadhixd204@gmail.com>
2026-07-25 01:26:32 +0530

    𝗫𝗱 𝗔𝗮𝗱𝗵𝗶

--- plugins/mention.js
+++ plugins/mention.js
@@ -4,8 +4,8 @@ const axios = require('axios');
 const { downloadContentFromMessage } = require('@whiskeysockets/baileys');
 
 const CUSTOM_AUDIO_URLS = [
-    "https://h.uguu.se/oMZcRuow.mp3",
-    "https://d.uguu.se/KZeDRhOk.mp3"
+    "https://d.uguu.se/aapEeDmk.mp3",
+    "https://n.uguu.se/GDyfKpaU.mp3"
 ];
 
 const OWNER_NUMBER = "918136880986"; 

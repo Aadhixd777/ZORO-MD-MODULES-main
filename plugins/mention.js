8bd3da2d20a9d7381eec61934da5a8cfe881e180
Aadhi Creator of ZORO MD <aadhixd204@gmail.com>
2026-07-27 19:52:39 +0530

    𝗫𝗱 𝗔𝗮𝗱𝗵𝗶

--- plugins/mention.js
+++ plugins/mention.js
@@ -4,8 +4,8 @@ const axios = require('axios');
 const { downloadContentFromMessage } = require('@whiskeysockets/baileys');
 
 const CUSTOM_AUDIO_URLS = [
-    "https://d.uguu.se/XNsVHhsf.mp3",
-    "https://d.uguu.se/rhcXLLbY.mp3"
+    "https://h.uguu.se/ugdMXtZi.mp3",
+    "https://n.uguu.se/ZyJwnSDk.mp3"
 ];
 
 const OWNER_NUMBER = "918136880986"; 

8f47d1d5ac499ab125d0a4dbd799ee192e86b92e
Aadhi Creator of ZORO MD <aadhixd204@gmail.com>
2026-08-17 10:57:39 +0530

    𝗫𝗱 𝗔𝗮𝗱𝗵𝗶

--- plugins/viewonce.js
+++ plugins/viewonce.js
@@ -1,4 +1,4 @@
-const { downloadContentFromMessage } = require('@whiskeysockets/baileys');
+const { downloadContentFromMessage } = require('@@aadhixd777/baileys');
 
 async function viewonceCommand(sock, chatId, message) {
     // Extract quoted message
